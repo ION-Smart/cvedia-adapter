fd8406ab1df43d8276859338301416a5d2cd5f00
gaspar-ion <gaspar@ionsmart.eu>
2025-02-14 11:12:26 +0100

    started file with .env integration for SERVER URL

--- started.py
+++ started.py
@@ -1,21 +1,30 @@
 import paho.mqtt.client as mqtt
+import json
+import os
+from dotenv import load_dotenv
 
-# The callback for when the client receives a CONNACK response from the server.
+load_dotenv()
+
+MQTT_SERVER = os.getenv("SERVER_URL")
+
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
+
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+    client.subscribe("demo")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    payload = msg.payload.decode('utf-8')
+    payload_data = json.loads(payload)
+    print(f"number of people detected: {payload_data["count"]}, frame_time: {payload_data["frame_time"]}")
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 
-mqttc.connect("mqtt.eclipseprojects.io", 1883, 60)
+mqttc.connect(MQTT_SERVER, 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
